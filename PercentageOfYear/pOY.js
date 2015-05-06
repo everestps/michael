f86f33746ed2e34aed278a32ed23e6c700634864
mvann <mvann.ep@mysummitps.org>
2015-05-06 05:40:04 +0000

    updating percentageOfYear

--- PercentageOfYear/pOY.js
+++ PercentageOfYear/pOY.js
@@ -1,8 +1,13 @@
-var timeBox = document.getElementById("timeBox");
-var begin = new Date(2015, 1, 1);
-var now = new Date();
-var difference = now - begin;
-var days = difference / 1000 / 3600 / 24;
-var percentage = days / 365 * 100;
-timeBox.innerHTML = (percentage.toFixed(2) + "%" + "<br>");
-timeBox.innerHTML = timeBox.innerHTML + (percentage + "%");
+window.requestAnimationFrame(displayPercentage);
+
+function displayPercentage(){
+  var timeBox = document.getElementById("timeBox");
+  var begin = new Date(2015, 1, 1);
+  var now = new Date();
+  var difference = now.getTime() - begin.getTime();
+  var days = difference / 1000 / 3600 / 24;
+  var percentage = days / 365 * 100;
+  timeBox.innerHTML = percentage.toFixed(2) + "%" + "<br>";
+  timeBox.innerHTML = timeBox.innerHTML + percentage.toFixed(6) + "%";
+  window.requestAnimationFrame(displayPercentage);
+}
